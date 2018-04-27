4e41b4802166d3c30644ca3c318ec35893b06ab8
TahjeiraCameron <36271884+TahjeiraCameron@users.noreply.github.com>
2018-04-27 11:19:44 -0400

    Added A6

---
+++ src/Assignment_6/A6Q1.java
@@ -0,0 +1,38 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment_6;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author camet2651
+ */
+public class A6Q1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("How many students are in the class ");
+        int classnum = input.nextInt();//get number of students in class
+
+        double[] students = new double[classnum];//make an array with a space for each students mark
+
+        System.out.println("Please enter the marks");
+
+        double sum = 0; //create sum
+
+        for (int i = 0; i < students.length; i++) {//go through array and enter each mark then add it to the mark before it
+            students[i] = input.nextDouble();
+            double marks = students[i];
+            sum = sum + marks;
+        }
+
+        double average = sum / classnum;     //find the average mark
+        System.out.println("The class average is " + (Math.round(average * 100.0) / 100.0) + "%"); //round to two decimal places     
+    }
+}
---
+++ src/Assignment_6/A6Q2.java
@@ -0,0 +1,45 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Assignment_6;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author camet2651
+ */
+public class A6Q2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double height = 0;
+        Scanner input = new Scanner(System.in);
+        System.out.println("How many people are there");
+        int num = input.nextInt();//get number of students in class
+
+        double[] people = new double[num];//make an array with a space for each height
+
+        System.out.println("Please enter the heights");
+
+        double sum = 0; //create sum
+
+        for (int i = 0; i < people.length; i++) {//go through array and enter each height then add it to the mark before it
+            people[i] = input.nextDouble();
+            double heights = people[i];
+            sum = sum + heights;
+        }
+
+        double average = sum / num;     //find the average heights
+
+        for (int i = 0; i < people.length; i++) {
+            if (people[i] > average) //see if height is greater than average then print it out if it is
+            {
+                System.out.println(people[i] + " is above the average height");
+            }
+        }
+    }
+}
